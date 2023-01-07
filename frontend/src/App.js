14d49abb55120f3d276010767c7e1ffc6a6289d7
Gyu-Park <kyesung8282@gmail.com>
2023-01-07 14:16:25 -0500

    added post function in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  
+  const [summarizedText, setData] = useState([]);
+  const [text, setText] = useState([]);
 
-  const [backendData, setBackendData] = useState([{}]);
+  function submit(e) {
+    e.preventDefault();
+    const textToSend = text;
 
-  useEffect(() => {
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data)
-      }
-    )
-  }, []);
+    if (textToSend === '') {
+      alert('Please add some text');
+      return; 
+    }
+    summarizeTextRequest(textToSend);
+  }
+
+  async function summarizeTextRequest(textToSend) {
+    try {
+      const response = await fetch('/api/summarizeText', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          data: textToSend
+        })
+      });
+
+      const res = await response.json();
+      console.log("data: " + res.data);
+      setData(res);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleText(e) {
+    const newText = e.target.value;
+    setText(newText);
+  }
 
   return (
-    <div className="App">
-      
-      
-    
+    <div>
+      <form>
+        <input onChange={(e) => handleText(e)} id="text" value={text} placeholder="enter texts to summarize" type="text"></input>
+        <button onClick={(e) => submit(e)}>Submit</button>
+      </form>
+      <div>
+        <p>summary: {summarizedText.data}</p>
+      </div>
     </div>
   );
 }
